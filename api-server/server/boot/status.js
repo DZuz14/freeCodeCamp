a74dffd9dba78a50a3dc2c606e5bf9c58006b8b1
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-03-23 18:15:57 +0100

    DEBUG: add wrapped errors on status/pang

--- api-server/server/boot/status.js
+++ api-server/server/boot/status.js
@@ -1,9 +1,18 @@
+import { wrapHandledError } from '../utils/create-handled-error';
+
 export default function bootStatus(app) {
   const api = app.loopback.Router();
 
   // DEBUG ROUTE
   api.get('/status/ping', (req, res) => {
     throw Error('debuging sentry');
+  });
+  api.get('/status/pang', (req, res) => {
+    throw wrapHandledError(Error('debuging sentry, wrapped'), {
+      type: 'info',
+      message: 'debugmessage',
+      redirectTo: `a/page`
+    });
   });
   app.use(api);
 }
